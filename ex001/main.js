db33568d4a1398727d644600b60b5fd2c702b3f2
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-13 17:23:04 -0300

    ex001

---
+++ ex001/main.js
@@ -0,0 +1,7 @@
+/* Exercício 1: Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores no console.*/
+
+let idade = 23;
+let peso = 73.5;
+
+console.log("Idade: ",idade);
+console.log("Peso: " + peso);
