cce49b716f9917e49924acfb73af9b3453719f43
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-14 11:35:13 -0300

    ex007

---
+++ ex007/main.js
@@ -0,0 +1,26 @@
+/*Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados*/
+
+let num1 = 2;
+let num2 = 4;
+let num3 = 6;
+
+console.log("Número 1 é maior que o número 2: ", num1 > num2);
+console.log("Número 1 é menor que o número 2: ", num1 < num2);
+console.log("Número 2 é maior que o número 3: ", num2 > num3);
+console.log("Número 2 é menor que o número 3: ", num2 < num3);
+console.log("Número 1 é maior que o número 3: ", num1 > num3);
+console.log("Número 1 é menor que o número 3: ", num1 < num3);
+
+console.log("Número 1 é maior ou igual ao número 2: ", num1 >= num2);
+console.log("Número 1 é menor ou igual ao número 2: ", num1 <= num2);
+console.log("Número 2 é maior ou igual ao número 3: ", num2 >= num3);
+console.log("Número 2 é menor ou igual ao número 3: ", num2 <= num3);
+console.log("Número 1 é maior ou igual ao número 3: ", num1 >= num3);
+console.log("Número 1 é menor ou igual ao número 3: ", num1 <= num3);
+
+console.log("Número 1 é igual ao número 2: ", num1 == num2);
+console.log("Número 1 é igual ao número 2: ", num1 == num2);
+console.log("Número 2 é igual ao número 3: ", num2 == num3);
+console.log("Número 2 é igual ao número 3: ", num2 == num3);
+console.log("Número 1 é igual ao número 3: ", num1 == num3);
+console.log("Número 1 é igual ao número 3: ", num1 == num3);
