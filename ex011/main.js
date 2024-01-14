d26e1c571aa559059640613a202111f1efc53e1c
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-14 18:02:22 -0300

    ex011

---
+++ ex011/main.js
@@ -0,0 +1,16 @@
+/*Escreva um programa que cerifica se uma pessoa pode votar com base na idade*/
+
+function verificaIdade(idade){
+    if(idade >= 16 && idade < 18){
+        console.log("O voto é opcional");
+    } else if (idade >= 18){
+        console.log("O voto é obrigatório");
+    } else {
+        console.log("O voto não é permitido");
+    }
+}
+
+verificaIdade(15);
+
+verificaIdade(16);
+verificaIdade(18);
