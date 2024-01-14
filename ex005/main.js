ae8ccc87833c515532ffa308af98309c354bce5e
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-14 11:16:15 -0300

    ex005

---
+++ ex005/main.js
@@ -0,0 +1,9 @@
+/*Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.*/
+
+let num1 = 3;
+let num2 = 9;
+let num3 = 5;
+
+let soma = num1 + num2 + num3;
+
+console.log("O resultado da soma é ", soma);
