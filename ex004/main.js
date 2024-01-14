c66df6d280fd8c3219fb9c9a49ac38a57c3061ca
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-14 11:10:07 -0300

    ex004

---
+++ ex004/main.js
@@ -0,0 +1,6 @@
+/* Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. */
+
+let nome = 'Rogerio';
+let sobrenome = 'Jesuino';
+
+console.log("Meu nome é ", nome, sobrenome);
