7e90cabe73313bb30f00dff9e5f27665ed7f06e8
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-14 11:40:32 -0300

    ex008

---
+++ ex008/main.js
@@ -0,0 +1,9 @@
+/*Declare uma variável e atribua um valor numérico a ela. Use o operador de incremento para aumentar o valor da variável. Imprima o resultado*/
+
+let num1 = 1;
+num1++;
+
+console.log("Somando um número: ", num1);
+
+num1--;
+console.log("Subtraindo um número: ", num1--);
