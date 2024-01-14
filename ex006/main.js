8a9b721e952afef7410eab06f2bf8b9fe9cdcb80
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-14 11:27:05 -0300

    ex006

---
+++ ex006/main.js
@@ -0,0 +1,8 @@
+/*Declare duas variáveis e atribua valores booleanos a elas. Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis. Imprima os resultados.*/
+
+let a = true;
+let b = false;
+
+console.log("AND: ", a && b);
+console.log("OR: ", a || b);
+console.log("NOT: ", !a);
