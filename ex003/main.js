8e749dd794c4b071cbe6b36527a18e7df927b5eb
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-14 11:05:57 -0300

    ex003

---
+++ ex003/main.js
@@ -0,0 +1,5 @@
+/* Declare uma variável chamada "número máximo", atribua a ela o valor máximo que um número pode ter em JavaScript e imprima o valor no console */
+
+let numMax = Number.MAX_VALUE;
+
+console.log("O valor máximo é ", numMax);
