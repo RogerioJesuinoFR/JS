a30e14dbf9ff0b3adef8420340391bd9c63cc74a
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-16 20:40:34 -0300

    ex025

---
+++ ex025/dezAUm.js
@@ -0,0 +1,11 @@
+/*Usando laço while, imprima os números de 10 a 1 (em ordem decrescente)*/
+
+function imprimeNumeros(){
+    let i = 10;
+    while(i >= 1){
+        console.log(i);
+        i--;
+    }
+}
+
+imprimeNumeros();
