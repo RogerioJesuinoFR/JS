1b786609fb8ce5d63c8dfa0e3516807ac704753d
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-17 19:05:58 -0300

    ex027

---
+++ ex027/tabuada5.js
@@ -0,0 +1,9 @@
+/*Usando um laço for imprima a tabuada de multiplicação do número 5*/
+
+function imprimeTabuadaCinco(){
+    for(let i = 1; i <= 10; i++){
+        console.log(i * 5);
+    }
+}
+
+imprimeTabuadaCinco();
