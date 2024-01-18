6f45aec604182d9d7376b4309c0134e664abc53d
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-17 21:15:25 -0300

    Update fizzBuzz.js

--- ex034/fizzBuzz.js
+++ ex034/fizzBuzz.js
@@ -6,8 +6,10 @@ while(i <= 100){
         console.log(i, " FizzBuzz");
     } else if (i % 3 === 0){
         console.log(i, " Fizz");
-    } else {
+    } else if (i % 5 === 0){
         console.log(i, " Buzz");
+    } else {
+        console.log(i);
     }
     i++;
 }
