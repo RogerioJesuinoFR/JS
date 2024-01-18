9ffe1cb6f188e5314beec30ec7d7255431fb26d4
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-17 21:20:51 -0300

    ex035

---
+++ ex035/numerosTriangulares.js
@@ -0,0 +1,7 @@
+/* usando um laço for, imprima os primeiros 10 números triangulares (n*(n+1)/2)*/
+
+for(let i = 1; i <= 10; i++){
+    let triangular = i;
+    triangular = i*(i +1)/2;
+    console.log(triangular); 
+}
