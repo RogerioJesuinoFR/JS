e3b754be89fd6b675c9cb8997e9793284b28baa7
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-18 19:29:01 -0300

    Update funcaoRetornaMaior.js

--- ex049/funcaoRetornaMaior.js
+++ ex049/funcaoRetornaMaior.js
@@ -2,6 +2,7 @@
 
 function retornaMaior(num1, num2, num3){
     return Math.max(num1, num2, num3);
+    //Essa função nativa do JS retorna o maior entre os números passados como parametros
 }
 
 console.log(`O maior entre os 3 números é ${retornaMaior(3,5,4)}`);
