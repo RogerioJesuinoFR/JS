a7ff1d1403afedcee39f4f8de7cf5c3d456dd51b
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-17 21:14:04 -0300

    ex034

---
+++ ex034/fizzBuzz.js
@@ -0,0 +1,13 @@
+/*Usando um laço while, imprima os números de 1 a 100, mas para múltiplos de 3 imprima "Fizz" e para múltiplos de 5 imprima "Buzz". Para números que são múltiplos de ambos, imprima "FizzBuzz" (Problema FizzBuzz)*/
+
+let i = 1;
+while(i <= 100){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i, " FizzBuzz");
+    } else if (i % 3 === 0){
+        console.log(i, " Fizz");
+    } else {
+        console.log(i, " Buzz");
+    }
+    i++;
+}
