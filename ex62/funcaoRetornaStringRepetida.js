8353efb2823db341f2c0002f1b548991531adf77
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-20 10:05:31 -0300

    ex062

---
+++ ex62/funcaoRetornaStringRepetida.js
@@ -0,0 +1,6 @@
+/*escreva uma função que receba um número e retorne uma string repetida aquele número de vezes*/
+
+function repeteString(num){
+    return 'Olá'.repeat(num);
+}
+console.log(repeteString(3));
