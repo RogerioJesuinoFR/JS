379a5c6350fab0bb7f97a42efaffe68f3f21a8a1
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-20 16:48:19 -0300

    Update funcaoArraysDeFuncoes.js

--- ex072/funcaoArraysDeFuncoes.js
+++ ex072/funcaoArraysDeFuncoes.js
@@ -3,6 +3,9 @@
 function aplicarCallbacksNTimes(n, callback, valorInicial){
     let valor = valorInicial;
     for(let i = 0;i < n;i++){
+
+        //A função aplicarCallbacksNTimes usa loop for para aplicar cada função de callback no valor atual n vezes. O valor resultante de uma função de callback é usado como entrada para a próxima função de callback. O valor resultante final é retornado.
+        
         callback.forEach(callback => {valor = callback(valor);});
     }
     return valor;
