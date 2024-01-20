cb5914e2efb38320dee652185ae691dd8669e688
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-20 15:01:29 -0300

    ex065

---
+++ ex065/funcaoCallback.js
@@ -0,0 +1,10 @@
+/*Escreva uma função que aceite um array de números e uma função de callback e retorne a soma de todos os números do array após a aplicação da função de callback*/
+
+function somaComCallback(array, callback){
+    return array.map(callback).reduce((a, b) => a + b, 0);
+}
+
+let numeros = [1,2,3,4,5];
+let callback = num => num * 2;
+
+console.log(somaComCallback(numeros, callback));
