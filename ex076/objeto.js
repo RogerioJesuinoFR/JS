5420a2719b46769bd30df0d87ee72e42eb2c9fd8
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2024-01-20 20:45:37 -0300

    ex076
    learning how to use object in JS

---
+++ ex076/objeto.js
@@ -0,0 +1,9 @@
+/*Cire um objeto vazio e adicione três propriedades a ele, cada uma com diferentes tipos de valores*/
+
+let obj = {};
+
+obj.numero = 10;
+obj.texto = "Olá, Mundo!";
+obj.booleano = true;
+
+console.log(obj);
